eb15778d2e1ac38e62bf35bb15daa40a8dec2390
Sviatoslav Khmelnychyi <pekachumon@gmail.com>
2022-12-14 21:54:51 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const P = styled.p`
+    color: #fff;
+    background: #000;
+    font-size: 50px;
+`;
+
+function App() {
+    return (
+        <P>Hey</P>
+    )
+}
+
+export default App;
